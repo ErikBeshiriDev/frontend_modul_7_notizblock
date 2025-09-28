d4bf54651ce648bae53e3f9ba6339751a279df58
ErikBeshiriDev <beshirierik1@gmail.com>
2025-09-28 17:12:02 +0200

    ### Updates: - index.html (update 2) - script.js (update 1)

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+let notes = ['banana','rasen mähen'];
+
+function renderNotes() {
+    let contentRef = document.getElementById('content')
+    contentRef.innerHTML = notes;
+}
+
+
+
+
+
+
+
+
+
+
+//notizen hinzufügen
+//notizen löschen
+//notizen archivieren
